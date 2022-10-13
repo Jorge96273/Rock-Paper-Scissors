09b59768c6e00f116282b735081accb7e0044bdf
Jorge96273 <115398843+Jorge96273@users.noreply.github.com>
2022-10-13 18:15:48 +0900

    Add files via upload

---
+++ Day 4 Finial-Rock, Paper, and Scissors.py
@@ -0,0 +1,56 @@
+rock = '''
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+'''
+
+paper = '''
+    _______
+---'   ____)____
+          ______)
+          _______)
+         _______)
+---.__________)
+'''
+
+scissors = '''
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+'''
+
+#Write your code below this line 👇
+#Users choice
+import random 
+options = [rock, paper, scissors]
+user_choice = input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.\n")
+user_choice1 = int(user_choice)
+output = options[user_choice1]
+print(output)
+#Computers random choice
+computer_choice = random.choice(options)
+print(computer_choice)
+#Possible Outcomes
+if output == computer_choice:
+  print("It is a tie!")
+if output == rock:
+  if computer_choice == paper:
+    print("Computer Wins!")
+  elif computer_choice == scissors:
+    print("Human Wins!")
+if output == paper:
+  if computer_choice == rock:
+    print("Human Wins!")
+  elif computer_choice == scissors:
+    print("Computer Wins!")
+if output == scissors:
+  if computer_choice == rock:
+    print("Computer Wins!")
+  elif computer_choice == paper:
+    print("Human Wins")
